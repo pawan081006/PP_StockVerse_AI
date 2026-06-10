d5a91547948c79346621c28861b1171773a586c2
Pawan Kumar <pawankumarlinkedin081006@gmail.com>
2026-06-10 11:06:53 +0530

    Spring Boot Backend Initialized

---
+++ Backend/stockverse-backend/src/main/java/com/ppstockverse/stockversebackend/StockverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ppstockverse.stockversebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockverseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockverseBackendApplication.class, args);
+    }
+
+}
